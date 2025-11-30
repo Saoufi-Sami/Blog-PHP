5e024271301a1c4577f47171c1714eded48a6aaa
saoufi sami <saoufi-sami@hotmail.fr>
2025-11-30 14:33:46 +0100

    Initial commit ou mise à jour du projet Blog

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+
+
+?>
+
+<form action="form.php" method="">
+<div>
+<label for="name">nom</label><br>
+<input type="text" name="name" id="name">
+</div>
+<br>
+<button>Submit</button>
+
+</form>
