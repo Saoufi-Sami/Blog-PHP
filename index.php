bdb6dbdc417af4c35e7ddec0a825c28f4e204b86
saoufi sami <saoufi-sami@hotmail.fr>
2026-01-20 14:49:21 +0100

    poo

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-$pdo = require_once 'database.php';
-$statement = $pdo->prepare('SELECT * FROM article');
-$statement->execute();
-$articles = $statement->fetchAll();
+$articleDB = require_once __DIR__ . '/database/models/ArticleDB.php';
+$articles = $articleDB->fetchAll();
 $categories = [];
 
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -42,34 +40,21 @@ if (count($articles)) {
 
 <body>
   <div class="container">
-      <?php require_once 'includes/header.php' ?>
-      <div class="content">
-        <div class="newsfeed-container">
-          <ul class="category-container">
-            <li class=<?= $selectedCat ? '' : 'cat-active' ?>><a href="/">Tous les articles <span class="small">(<?= count($articles) ?>)</span></a></li>
-            <?php foreach ($categories as $catName => $catNum) : ?>
-              <li class=<?= $selectedCat ===  $catName ? 'cat-active' : '' ?>><a href="/?cat=<?= $catName ?>"> <?= $catName ?><span class="small">(<?= $catNum ?>)</span> </a></li>
-            <?php endforeach; ?>
-          </ul>
-          <div class="newsfeed-content">
-            <?php if (!$selectedCat) : ?>
-              <?php foreach ($categories as $cat => $num) : ?>
-                <h2><?= $cat ?></h2>
-                <div class="articles-container">
-                  <?php foreach ($articlePerCategories[$cat] as $a) : ?>
-                    <a href="/show-article.php?id=<?= $a['id'] ?>" class="article block">
-                      <div class="overflow">
-                        <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
-                      </div>
-                      <h3><?= $a['title'] ?></h3>
-                    </a>
-                  <?php endforeach; ?>
-                </div>
-              <?php endforeach; ?>
-            <?php else : ?>
-              <h2><?= $selectedCat ?></h2>
+    <?php require_once 'includes/header.php' ?>
+    <div class="content">
+      <div class="newsfeed-container">
+        <ul class="category-container">
+          <li class=<?= $selectedCat ? '' : 'cat-active' ?>><a href="/">Tous les articles <span class="small">(<?= count($articles) ?>)</span></a></li>
+          <?php foreach ($categories as $catName => $catNum) : ?>
+            <li class=<?= $selectedCat ===  $catName ? 'cat-active' : '' ?>><a href="/?cat=<?= $catName ?>"> <?= $catName ?><span class="small">(<?= $catNum ?>)</span> </a></li>
+          <?php endforeach; ?>
+        </ul>
+        <div class="newsfeed-content">
+          <?php if (!$selectedCat) : ?>
+            <?php foreach ($categories as $cat => $num) : ?>
+              <h2><?= $cat ?></h2>
               <div class="articles-container">
-                <?php foreach ($articlePerCategories[$selectedCat] as $a) : ?>
+                <?php foreach ($articlePerCategories[$cat] as $a) : ?>
                   <a href="/show-article.php?id=<?= $a['id'] ?>" class="article block">
                     <div class="overflow">
                       <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
@@ -78,7 +63,20 @@ if (count($articles)) {
                   </a>
                 <?php endforeach; ?>
               </div>
-            <?php endif; ?>
+            <?php endforeach; ?>
+          <?php else : ?>
+            <h2><?= $selectedCat ?></h2>
+            <div class="articles-container">
+              <?php foreach ($articlePerCategories[$selectedCat] as $a) : ?>
+                <a href="/show-article.php?id=<?= $a['id'] ?>" class="article block">
+                  <div class="overflow">
+                    <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                  </div>
+                  <h3><?= $a['title'] ?></h3>
+                </a>
+              <?php endforeach; ?>
+            </div>
+          <?php endif; ?>
         </div>
       </div>
     </div>
