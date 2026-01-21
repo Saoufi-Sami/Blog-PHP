9f3969af37def6f2068cbed857e7beaf5d0c4b25
saoufi sami <saoufi-sami@hotmail.fr>
2026-01-21 10:08:49 +0100

    Connexion

--- auth-register.php
+++ auth-register.php
@@ -1,5 +1,10 @@
 <?php
+$pdo = require_once './database/database.php';
 const ERROR_REQUIRED = 'Veuillez renseigner ce champ';
+const ERROR_TOO_SHORT = 'Ce champ est trop court';
+const ERROR_PASSWORD_TOO_SHORT = 'Le mot de passe doit faire au moins 6 caractères';
+const ERROR_PASSWORD_MISMATCH = 'Le mot de passe de confirmation est différent';
+const ERROR_EMAIL_INVALID = 'L\'email n\'est pas valide';
 $errors = [
   'firstname' => '',
   'lastname' => '',
@@ -9,8 +14,57 @@ $errors = [
 ];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $input = filter_input_array(INPUT_POST, [
+    'firstname' => FILTER_SANITIZE_SPECIAL_CHARS,
+    'lastname' => FILTER_SANITIZE_SPECIAL_CHARS,
+    'email' => FILTER_SANITIZE_EMAIL,
+  ]);
+  $firstname = $input['firstname'] ?? '';
+  $lastname = $input['lastname'] ?? '';
+  $email = $input['email'] ?? '';
+  $password = $_POST['password'] ?? '';
+  $confirmpassword = $_POST['confirmpassword'] ?? '';
+
+  if (!$firstname) {
+    $errors['firstname'] = ERROR_REQUIRED;
+  } elseif (mb_strlen($firstname) < 2) {
+    $errors['firstname'] = ERROR_TOO_SHORT;
+  }
+  if (!$lastname) {
+    $errors['lastname'] = ERROR_REQUIRED;
+  } elseif (mb_strlen($lastname) < 2) {
+    $errors['lastname'] = ERROR_TOO_SHORT;
+  }
+  if (!$email) {
+    $errors['email'] = ERROR_REQUIRED;
+  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors['email'] = ERROR_EMAIL_INVALID;
+  }
+  if (!$password) {
+    $errors['password'] = ERROR_REQUIRED;
+  } elseif (mb_strlen($password) < 6) {
+    $errors['password'] = ERROR_PASSWORD_TOO_SHORT;
+  }
+  if (!$confirmpassword) {
+    $errors['confirmpassword'] = ERROR_REQUIRED;
+  } elseif ($confirmpassword !== $password) {
+    $errors['confirmpassword'] = ERROR_PASSWORD_MISMATCH;
+  }
 
   if (empty(array_filter($errors, fn ($e) => $e !== ''))) {
+    $statement = $pdo->prepare('INSERT INTO user VALUES (
+      DEFAULT,
+      :firstname,
+      :lastname,
+      :email,
+      :password
+    )');
+    $hashedPassword = password_hash($password, PASSWORD_ARGON2ID);
+    $statement->bindValue(':firstname', $firstname);
+    $statement->bindValue(':lastname', $lastname);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':password', $hashedPassword);
+    $statement->execute();
     header('Location: /');
   }
 }
