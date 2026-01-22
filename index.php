c6b4260255b00447b9a822cb90037e0c78afdb6e
saoufi sami <saoufi-sami@hotmail.fr>
2026-01-22 11:29:00 +0100

    Deconnexion

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+require __DIR__ . '/database/database.php';
+require __DIR__ . '/database/security.php';
+
+$currentUser = isLoggedin();
 $articleDB = require_once __DIR__ . '/database/models/ArticleDB.php';
 $articles = $articleDB->fetchAll();
 $categories = [];
